c2bc50926f9f6f09f2bdbd3b5bad9736d32ccde2
darinzahira <56281751+darinzahira@users.noreply.github.com>
2020-04-07 11:19:39 +0700

    jobsheet 9

---
+++ source code/laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Profil Saya</title>
+</head>
+<body>
+<h1>Profil Saya</h1>            
+<p>Perkenalkan Nama saya Darin Zahira, saya seorang Mahasiswi D-IV Teknik Informatika, 
+Jurusan Teknologi Informasi, Politeknik Negeri Malang</p> 
+</body>
+</html>
