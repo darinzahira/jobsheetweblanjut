350ba67294ac4741e743e514b3c80fa53a7858ad
darinzahira <56281751+darinzahira@users.noreply.github.com>
2020-04-14 15:01:18 +0700

    jobsheet 10

---
+++ source code/laravel-crud-kedua/resources/views/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('master')
+
+@section('title', 'Edit Data')
+
+@section('judul_halaman', 'Edit Data Mahasiswa')
+
+@section('konten')
+    <a href="/mahasiswa" class="btn btn-danger">Kembali</a>
+    <br/>
+    <br/>
+    <form action="/mahasiswa/update/{{ $mahasiswa->id }}" method="post">
+        {{ csrf_field() }}
+        <input type="hidden" name="id" value="{{ $mahasiswa->id }}"> <br/>
+        <div class="form-group">
+            <label for="namamhs">Nama</label>
+            <input type="text" class="form-control" required="required" name="namamhs" value="{{ $mahasiswa->nama }}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="nimmhs">NIM</label>
+            <input type="number" class="form-control" required="required" name="nimmhs" value="{{ $mahasiswa->nim }}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="emailmhs">E-mail</label>
+            <input type="email" class="form-control" required="required" name="emailmhs" value="{{ $mahasiswa->email }}"><br/>
+        </div>
+        <div class="form-group">
+            <label for="jurusanmhs">Jurusan</label>
+            <input type="text" class="form-control" required="required" name="jurusanmhs" value="{{ $mahasiswa->jurusan }}">
+        </div>
+        <button type="submit" name="tambah" class="btn btn-primary float-right">Simpan Data</button>
+    </form>
+@endsection
