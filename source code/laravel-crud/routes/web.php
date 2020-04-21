0c00c17ec8a6241d6e279814949d42c8d174970f
darinzahira <56281751+darinzahira@users.noreply.github.com>
2020-04-21 11:27:18 +0700

    jobsheet 11

--- source code/laravel-crud/routes/web.php
+++ source code/laravel-crud/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/mahasiswa', 'MahasiswaController@index');
 
@@ -30,4 +30,11 @@ Route::get('/mahasiswa/edit/{id}', 'MahasiswaController@edit');
 Route::post('/mahasiswa/update', 'MahasiswaController@update');
 
 Route::get('/mahasiswa/hapus/{id}', 'MahasiswaController@hapus');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/admin/users', 'Admin\UserController',['except' =>['show','create','store']]);
 
