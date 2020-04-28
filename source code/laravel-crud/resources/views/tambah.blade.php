29d1ab286dc237299279c406b73447db68928121
darinzahira <56281751+darinzahira@users.noreply.github.com>
2020-04-28 11:22:43 +0700

    jobsheet 13

--- source code/laravel-crud/resources/views/tambah.blade.php
+++ source code/laravel-crud/resources/views/tambah.blade.php
@@ -8,24 +8,35 @@
     <a href="/mahasiswa" class="btn btn-danger">Kembali</a>
     <br/>
     <br/>
+    @if (count($errors) > 0)
+    <div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    </div>
+    @endif
     <form action="/mahasiswa/simpan" method="post">
         {{ csrf_field() }} 
         <div class="form-group">
-            <label for="namamhs">Nama</label>
-            <input type="text" class="form-control" required="required" name="namamhs"><br/>
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" name="nama" value="{{ old('nama') }}">
         </div>
         <div class="form-group">
-            <label for="nimmhs">NIM</label>
-            <input type="number" class="form-control" required="required" name="nimmhs"><br/>
+            <label for="nim">NIM</label>
+            <input type="text" class="form-control" name="nim" value="{{ old('nim') }}">
         </div>
         <div class="form-group">
-            <label for="emailmhs">E-mail</label>
-            <input type="email" class="form-control" required="required" name="emailmhs"><br/>
+            <label for="email">E-mail</label>
+            <input type="text" class="form-control" name="email" value="{{ old('email') }}">
         </div>
         <div class="form-group">
-            <label for="jurusanmhs">Jurusan</label>
-            <input type="text" class="form-control" required="required" name="jurusanmhs"><br/>
+            <label for="jurusan">Jurusan</label>
+            <input type="text" class="form-control" name="jurusan" value="{{ old('jurusan') }}">
         </div>
-        <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+        <div class="form-group">
+            <input class="btn btn-primary" type="submit" value="Tambah">
+        </div>
     </form>
 @endsection
